3f274b71c275885ef5679d5907e907d4b3d97bf4
Jason King <jaceace28@googlemail.com>
2018-01-11 11:22:35 +0000

    Commit
    Update.

--- db/migrate/20170422201228_sample_name_change_column_type.rb
+++ db/migrate/20170422201228_sample_name_change_column_type.rb
@@ -1,6 +1,6 @@
 class SampleNameChangeColumnType < ActiveRecord::Migration
   def change
-    change_column(:fixtures, :hteam, USING :hteam::integer)
-    change_column(:fixtures, :ateam, USING :ateam::integer)
+  change_column :fixtures, :hteam, 'text using hteam::text'
+  change_column :fixtures, :ateam, 'text using ateam::text'
 end
 end
