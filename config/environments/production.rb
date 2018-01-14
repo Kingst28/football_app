cd679bf7abfe804bfa0115695bc2cd14ce063af2
Jason King <jaceace28@googlemail.com>
2018-01-14 10:35:15 +0000

    Update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
   
-  Sunspot.config.solr.url = 'config/production.rb'
+  #Sunspot.config.solr.url = 'config/production.rb'
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
