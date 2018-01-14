78b2e7fe12837d9d68e23017eb9d7ccfaebb86c1
Jason King <jaceace28@googlemail.com>
2018-01-14 10:41:33 +0000

    Update

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,8 +1,8 @@
 class Player < ActiveRecord::Base
 	belongs_to :team
 	has_many :bids
-	searchable do
-		text :name
-		integer :teams_id
-	end
+	#searchable do
+		#text :name
+		#integer :teams_id
+	#end
 end
