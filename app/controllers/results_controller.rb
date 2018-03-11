39f11aba8fd69ea6d9e0a564c4ec2d8686f1be27
Jason King <jaceace28@googlemail.com>
2018-03-11 13:15:30 +0000

    update

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -78,10 +78,9 @@ class ResultsController < ApplicationController
       end
 
       for p in @teamsheet_conceders do
-      if p.player.position == 'Defender' || p.player.position == 'Goalkeeper' && p.read_attribute(:played) == true then
+      if p.player.position == "Defender" || p.player.position == "Goalkeeper" && p.read_attribute(:played) == true then
         defenderCount = defenderCount + 1
-      elsif p.player.position == 'Defender' || p.player.position == 'Goalkeeper' && p.read_attribute(:played) == false then
-        defenderCount = defenderCount
+      elsif p.player.position == "Defender" || p.player.position == "Goalkeeper" && p.read_attribute(:played) == false then
       end
     end
     if defenderCount == 0 then
