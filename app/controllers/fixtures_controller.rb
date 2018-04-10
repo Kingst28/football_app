7b8c23e7685b513a1ebd510593640e39295e6d58
Jason King <jaceace28@googlemail.com>
2018-04-10 08:59:11 +0100

    update

--- app/controllers/fixtures_controller.rb
+++ app/controllers/fixtures_controller.rb
@@ -5,7 +5,7 @@ class FixturesController < ApplicationController
   # GET /fixtures.json
   def index
     all_fixtures = Fixture.all
-    @fixtures = all_fixtures.group_by(&:matchday).order(:matchday)
+    @fixtures = all_fixtures.group_by(&:matchday).sort_by{|matchday, _fixtures| matchday.sort }
     @notifications_all = Notification.where(:user_id => current_user.id)
   end
 
