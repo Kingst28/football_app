5dafc8d45f4b24ba21eece99c5123f96c1175ac5
Jason King <jaceace28@googlemail.com>
2018-07-15 15:51:02 +0100

    update

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -14,6 +14,7 @@ class PlayersController < ApplicationController
   		@defenders = @players.where(:position => 'Defender')
   		@midfielders = @players.where(:position => 'Midfielder')
   		@strikers = @players.where(:position => 'Striker')
+	        @notifications = Notification.all
 	end
 
  def new
