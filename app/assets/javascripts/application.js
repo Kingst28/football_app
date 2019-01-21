9c8514657c49d9e82949c0dca4bea7b2ab6b6403
Kingst28 <jaceace28@googlemail.com>
2019-01-21 18:11:10 +0000

    update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,10 +9,10 @@
 //
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
-//= require turbolinks
 //= require jquery
-//= require jquery.turbolinks
 //= require jquery_ujs
+//= require jquery.turbolinks
+//= require turbolinks
 //= require jquery.matchcenter
 //= require datetime_picker_input
 //= manifest.json
