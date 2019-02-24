e7d025403e564b44d7f561f724520ffedf7798f7
Kingst28 <jaceace28@googlemail.com>
2019-02-24 19:07:27 +0000

    update

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,5 +1,3 @@
-Rails.configuration.assets.precompile += %w[javascripts/manifest.json]
-Rails.configuration.assets.precompile += %w[serviceworker.js manifest.json]
 Rails.application.configure do
   config.assets.precompile += %w[serviceworker.js manifest.json]
 end
