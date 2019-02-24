4a68e3e02c994e220e4db9cd83804f98c7c4a90d
Kingst28 <jaceace28@googlemail.com>
2019-02-24 20:01:56 +0000

    update

--- config/initializers/serviceworker.rb
+++ config/initializers/serviceworker.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   config.serviceworker.routes.draw do
     # map to assets implicitly
-    match "/serviceworker.js"
+    match "/serviceworker.js" => "<%= asset_path 'serviceworker.js' %>"
     match "/manifest.json"
 end
 end
