630edde38b38422024b466e9c48fcd86cc9ef8b9
Kingst28 <jaceace28@googlemail.com>
2020-01-08 19:24:17 +0000

    update associations

--- db/schema.rb
+++ db/schema.rb
@@ -138,6 +138,7 @@ ActiveRecord::Schema.define(version: 20191020191549) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
+
 # Could not dump table "teams" because of following NoMethodError
 #   undefined method `[]' for nil:NilClass
 
