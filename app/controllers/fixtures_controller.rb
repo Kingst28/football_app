42c5d95487be18cd0eef7d2cb839b7a06a606028
Kingst28 <jaceace28@googlemail.com>
2020-05-20 21:30:00 +0100

    update

--- app/controllers/fixtures_controller.rb
+++ app/controllers/fixtures_controller.rb
@@ -7,7 +7,7 @@ class FixturesController < ApplicationController
   # GET /fixtures.json
   def index
     all_fixtures = Fixture.all
-    @fixtures = all_fixtures.group_by(&:matchday).order(haflag: :desc)
+    @fixtures = all_fixtures.group_by(&:matchday).sort_by{ |haflag| haflag }.reverse
     @notifications_all = Notification.where(:user_id => current_user.id)
   end
 
