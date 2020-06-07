0a5c872fec4654f9adc8b8af3e4ebd3035079069
Kingst28 <jaceace28@googlemail.com>
2020-06-07 20:38:37 +0100

    update

--- app/controllers/results_masters_controller.rb
+++ app/controllers/results_masters_controller.rb
@@ -76,7 +76,7 @@ class ResultsMastersController < ApplicationController
   end
 
   def edit_multiple
-    @results_masters = ResultsMaster.find(params[:results_masters_ids]).order(Team.where(:id => Player.where(:playerteam => self[:name]).pluck(:teams_id)).pluck(:name)[0])
+    @results_masters = ResultsMaster.find(params[:results_masters_ids]).order(:name)
     @notifications_all = Notification.all
   end
 
