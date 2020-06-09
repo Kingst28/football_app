d6dcc9da8149c99d3475212497eb127ee0df58a4
Kingst28 <jaceace28@googlemail.com>
2020-06-09 19:39:50 +0100

    update

--- app/controllers/results_masters_controller.rb
+++ app/controllers/results_masters_controller.rb
@@ -76,7 +76,7 @@ class ResultsMastersController < ApplicationController
   end
 
   def edit_multiple
-    @results_masters = ResultsMaster.where(:id => params[:results_masters_ids]).order('SUBSTRING_INDEX("name", "-", -2)')
+    @results_masters = ResultsMaster.where(:id => params[:results_masters_ids]).order("SUBSTRING_INDEX(name, '-', -2)")
     @notifications_all = Notification.all
   end
 
