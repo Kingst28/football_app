de62f17aa325bce2cc48b5efe7860cb84e9bdc2e
Kingst28 <jaceace28@googlemail.com>
2020-10-29 11:45:12 +0000

    update

--- app/controllers/teamsheet_controller.rb
+++ app/controllers/teamsheet_controller.rb
@@ -12,18 +12,19 @@ class TeamsheetController < ApplicationController
     @current_matchday = Matchday.where(:account_id => current_user.account_id)
     @fixture = Fixture.where(:matchday => @current_matchday.first.matchday_number).where(:haflag => @current_matchday.first.haflag).where(:account_id => @current_matchday.first.account_id)
 
-    defenderCount = Teamsheet.where(:user_id => current_user.id).where(:played => true).joins(:player).where("position = 'Goalkeeper' OR position = 'Defender'").count
-    total_scorenum = Teamsheet.where(:user_id => current_user.id).where(:active => true).sum(:scorenum).to_s.tr('""','').tr('[]','').to_i
-    total_connum = Teamsheet.where(:user_id => current_user.id).where(:active => true).sum(:concedednum).to_s.tr('""','').tr('[]','').to_i
-    @final_score = []
-    if defenderCount = 0 then 
-    else
-      con_score1 = 5 - defenderCount
-      con_score2 = total_connum / defenderCount
-      final_con_score = con_score1 + con_score2
-      con_score = final_con_score * -1
-      @final_score << total_scorenum + con_score
-    end
+    #defenderCount = Teamsheet.where(:user_id => current_user.id).where(:played => true).joins(:player).where("position = 'Goalkeeper' OR position = 'Defender'").count
+    #total_scorenum = Teamsheet.where(:user_id => current_user.id).where(:active => true).sum(:scorenum).to_s.tr('""','').tr('[]','').to_i
+    #total_connum = Teamsheet.where(:user_id => current_user.id).where(:active => true).sum(:concedednum).to_s.tr('""','').tr('[]','').to_i
+    #@final_score = []
+    
+    #if defenderCount = 0 then 
+    #else
+      #con_score1 = 5 - defenderCount
+      #con_score2 = total_connum / defenderCount
+      #final_con_score = con_score1 + con_score2
+      #con_score = final_con_score * -1
+      #@final_score << total_scorenum + con_score
+    #end
     
     @goalkeeper = []
     @defender = []
