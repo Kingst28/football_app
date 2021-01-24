1494e9010c17639ba42e4d8adee1981ea18a6e6d
Kingst28 <jaceace28@googlemail.com>
2021-01-24 16:36:24 +0000

    update

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -977,6 +977,7 @@ class SessionsController < ApplicationController
           elsif player_position == 'Defender'
             @bid = Bid.joins(:player).where("user_id = ? AND players.position = ? AND bids.transfer_out = ?", o.user_id, "Defender", true).order(updated_at: :desc)
             @teamsheet = Teamsheet.joins(:player).where("user_id = ? AND player_id = ?", o.user_id, o.player_id).order(updated_at: :desc)
+            if @bid.exists?
             for bid in @bid do
               bid_id = bid.read_attribute(:id)
               player_id = bid.read_attribute(:player_id)
@@ -990,6 +991,7 @@ class SessionsController < ApplicationController
               @bidDelete = Bid.find(bid_id).delete
               @playerTaken = Player.find(player_id).update_attribute(:taken, "No")
               @teamsheet_new.assign_attributes(:active => active, :priority => priority)
+            end
             end
             defender_pri(defender_count, @defender_priority1_count, @defender_priority2_count, o.user_id, o.player_id, o.player.playerteam, o.amount, o.account_id)
           elsif player_position == 'Midfielder'
