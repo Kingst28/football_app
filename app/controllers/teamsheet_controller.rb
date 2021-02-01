962b454dd3f16e99e24cb8a1528503b0aa1e07bd
Kingst28 <jaceace28@googlemail.com>
2021-02-01 22:09:27 +0000

    update

--- app/controllers/teamsheet_controller.rb
+++ app/controllers/teamsheet_controller.rb
@@ -35,6 +35,22 @@ class TeamsheetController < ApplicationController
         elsif finalscore[1].to_i == finalscore[0].to_i then
           @results << "D"
         end
+      elsif result.haflag == "Home" && result.ateam == current_user.id.to_s then
+        if finalscore[1].to_i > finalscore[0].to_i then
+          @results << "W"
+        elsif finalscore[1].to_i < finalscore[0].to_i then
+          @results << "L"
+        elsif finalscore[1].to_i == finalscore[0].to_i then
+          @results << "D"
+        end
+      elsif result.haflag == "Away" && result.hteam == current_user.id.to_s then
+        if finalscore[0].to_i > finalscore[1].to_i then
+          @results << "W"
+        elsif finalscore[0].to_i < finalscore[1].to_i then
+          @results << "L"
+        elsif finalscore[0].to_i == finalscore[1].to_i then
+          @results << "D"
+        end
       end
     end
     
