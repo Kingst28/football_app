2686c60ca47cef06ab439f3c80a5ee841caea1f6
Kingst28 <jaceace28@googlemail.com>
2021-02-14 08:04:10 +0000

    update

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -980,7 +980,7 @@ class SessionsController < ApplicationController
             end
             gk_pri(goalkeeper_count, @goalkeeper_priority1_count, o.user_id, o.player_id, o.player.playerteam, o.amount, o.account_id)
           elsif player_position == "Defender"
-            @bid = Bid.joins(:player).where("user_id = ? AND players.position = ? AND bids.transfer_out = ?", o.user_id, "Defender", true).order(transfer_out: :asc)
+            @bid = Bid.joins(:player).where("bids.user_id = ? AND players.position = ? AND bids.transfer_out = ? AND bids.account_id = ?", o.user_id, "Defender", true, o.account_id).order(updated_at: :desc)
             if @bid.exists?
               for bid in @bid do 
                 bid_id = bid.read_attribute(:id)
