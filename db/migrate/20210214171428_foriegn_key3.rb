2139975a45257632aef9e0d64b35eb94da442ef6
Kingst28 <jaceace28@googlemail.com>
2021-02-14 17:18:01 +0000

    update

--- db/migrate/20210214171428_foriegn_key3.rb
+++ db/migrate/20210214171428_foriegn_key3.rb
@@ -1,5 +1,5 @@
 class ForiegnKey3 < ActiveRecord::Migration
   def change
-    remove_foreign_key :bids, :teamsheets
+    remove_foreign_key :bids, :teamsheets, column: :player_id, on_delete: :cascade
   end
 end
